0c542d53f3a94251048ecf4bafc84654fd294df3
Kyle Rhyner <klrhyner@u.washington.edu>
2020-10-24 17:43:05 -0700

    making heads and tails of the provided files

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 import requests
 from flask import Flask, send_file, Response
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 
 app = Flask(__name__)
 
@@ -10,12 +10,16 @@ app = Flask(__name__)
 def get_fact():
 
     response = requests.get("http://unkno.com")
-
-    soup = BeautifulSoup(response.content, "html.parser")
+    # response.content is the whole page in html format with \n and \r
+    soup = bs(response.content, "html.parser")
+    # soup is now the human readable format of the html doc
     facts = soup.find_all("div", id="content")
+    # facts is now a list of the found items
 
+    # alternatively coud do just a .find(); this does not make a list like .find_all()
+    # fact = soup.find('div', id='content')
     return facts[0].getText()
-
+    # return fact.getText().strip()
 
 @app.route('/')
 def home():
